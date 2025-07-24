63f06e3627d9eae364e603b00bae0311959f6f0b
Sage-tech <Sierra.s.sage@gmail.com>
2025-07-24 11:46:02 -0700

    ts

--- TS-BASICS/functions-test.ts
+++ TS-BASICS/functions-test.ts
@@ -1,6 +1,11 @@
 // this is where we will import our functions to test them
 
 // named export import addNumbers, {}
-import addNumbers from './functions'
-console.log(addNumbers(1,2))
-console.log(addNumbers(1, "Jack"))
+// import addNumbers from './functions'
+// console.log(addNumbers(1,2))
+
+// Destructured imports
+import addNumbers, {addStrings} from './functions'
+console.log(addNumbers(1, 2))
+console.log(addStrings("a", "b"));
+console.log(addStrings("a"));
--- TS-BASICS/functions.ts
+++ TS-BASICS/functions.ts
@@ -1,9 +1,24 @@
 // definine functions
 // this function takes 2 values 
-function addNumbers(a, b) {
+function addNumbers(a: number, b:number): number {
     return a + b;
 }
 
 // step to import your functions to another file 
 export default addNumbers;
 // as the default export from this module
+
+// declaring your funtions CONST functions
+export const addStrings = (str1: string, str2: string = ""): string => `${str1} ${str2}`; 
+                                        // str2: string is the declaration so after we can set it to the default value 
+
+//Union Types
+// a function where you want a certain parameter
+// this parameter is to be of one of a set of different types
+
+export const format = (title: string, param: string | number ): string => 
+    `${title} ${param}`;
+// void functions = where you are not actually going to return anything at all
+export const printFormat = (title: string, param: string | number) => {
+    console.log(format(title,param))
+}
